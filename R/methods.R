05c58cb331b953fa975482898e4c705d918ed002
Greg Ridgeway <gregridgeway@gmail.com>
2026-04-18 08:07:10 -0400

    define a summary() method

--- R/methods.R
+++ R/methods.R
@@ -12,8 +12,8 @@
 #' @return
 #' \code{print.cosmic_fit()} returns \code{x} invisibly.
 #'
-#' \code{summary.cosmic_fit()} returns the summary matrix produced by
-#' \code{summary(object$fit)$summary}.
+#' \code{summary.cosmic_fit()} returns the summary matrix produced by the
+#' underlying \code{stanfit} summary method.
 #'
 #' @name summary.cosmic_fit
 #' @rdname summary.cosmic_fit
@@ -25,5 +25,54 @@ print.cosmic_fit <- function(x, ...) {
 #' @rdname summary.cosmic_fit
 #' @export
 summary.cosmic_fit <- function(object, ...) {
-  summary(object$fit)$summary
+  .stan_summary_matrix(object$fit, ...)
+}
+
+.stan_summary_matrix <- function(fit, ...) {
+  summary_candidates <- list(
+    fit,
+    tryCatch(rstan::summary(fit, ...), error = function(e) NULL),
+    tryCatch(summary(fit, ...), error = function(e) NULL)
+  )
+
+  for (fit_summary in summary_candidates) {
+    summary_matrix <- .extract_summary_matrix(fit_summary)
+    if (!is.null(summary_matrix)) {
+      return(summary_matrix)
+    }
+  }
+
+  stop("Could not extract a summary matrix from the stanfit object.", call. = FALSE)
+}
+
+.extract_summary_matrix <- function(x) {
+  if (is.null(x)) {
+    return(NULL)
+  }
+
+  if (is.matrix(x)) {
+    return(.normalize_summary_matrix(x))
+  }
+
+  if (is.list(x) && !is.null(x$summary) && is.matrix(x$summary)) {
+    return(.normalize_summary_matrix(x$summary))
+  }
+
+  if (is.list(x)) {
+    matrix_elements <- vapply(x, is.matrix, logical(1))
+    if (any(matrix_elements)) {
+      return(.normalize_summary_matrix(x[[which(matrix_elements)[1]]]))
+    }
+  }
+
+  NULL
+}
+
+.normalize_summary_matrix <- function(x) {
+  attributes_to_keep <- attributes(x)
+  attributes_to_keep$class <- NULL
+
+  out <- unclass(x)
+  attributes(out) <- attributes_to_keep
+  out
 }
