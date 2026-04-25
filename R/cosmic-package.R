16c928579f93d26270bdb191c587b3246f1b4bb5
Greg Ridgeway <gregridgeway@gmail.com>
2026-04-25 17:23:11 -0400

    cleanup

--- R/cosmic-package.R
+++ R/cosmic-package.R
@@ -2,14 +2,8 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @import methods
-#' @import Rcpp
-#' @importFrom RcppParallel RcppParallelLibs
-#' @importFrom rstan sampling
-#' @importFrom rstantools rstan_config
 #' @importFrom stats sd runif
 ## usethis namespace: end
-#' @useDynLib cosmic, .registration = TRUE
 NULL
 
 utils::globalVariables(c(
